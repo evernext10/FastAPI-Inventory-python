287233973027924e2497df4dd777162b8938da91
Evert Moreno <evernext10@gmail.com>
2022-11-23 13:03:00 -0500

    Setup timing and logger

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-from fastapi import Depends, FastAPI
+import asyncio
+import logging
+import uvicorn
+
+from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+
 from strawberry import Schema
 
 from configs.environment import get_environment_variables
 from models.BaseModel import init
 from routers.v1.ProductRouter import ProductRouter
 
+from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.responses import PlainTextResponse
+from timing_asgi import TimingMiddleware, TimingClient
+from timing_asgi.integrations import StarletteScopeToName
+
+
+class PrintTimings(TimingClient):
+    def timing(self, metric_name, timing, tags):
+        print(metric_name, timing, tags)
+
+
 # Application Environment Configuration
 env = get_environment_variables()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+timing = Starlette()
+timing.add_middleware(
+    TimingMiddleware,
+    client=PrintTimings(),
+    metric_namer=StarletteScopeToName(prefix="app", starlette_app=timing)
+)
 
 # Core Application Instance
 app = FastAPI(
     title=env.APP_NAME,
     version=env.API_VERSION
 )
+
+
+static_files_app = StaticFiles(directory=".")
+app.mount(path="/static", app=static_files_app, name="static")
 
 # Add Routers
 app.include_router(ProductRouter)
@@ -37,3 +68,7 @@ def custom_openapi():
     return app.openapi_schema
 
 app.openapi = custom_openapi
+
+
+if __name__ == "__main__":
+    uvicorn.run(timing)
